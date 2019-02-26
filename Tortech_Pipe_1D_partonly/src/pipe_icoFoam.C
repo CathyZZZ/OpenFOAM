30e602c6e37252340d09e4af7ff85867f1969006
Cathy Zhu <ziyizhu00@gmail.com>
2019-02-26 00:41:45 +0000

    Wrote 1D case called Tortech_Pipe_1D
    v2=v1+cx, proved correct.

--- Tortech_Pipe_1D_partonly/src/pipe_icoFoam.C
+++ Tortech_Pipe_1D_partonly/src/pipe_icoFoam.C
@@ -58,9 +58,12 @@ int main(int argc, char *argv[])
         // Momentum predictor
 //        #include "createFields.H"
           #include "solveMomentum.H"
-          #include "solveEnergy.H"
+ //         #include "solveEnergy.H"
+//          #include "solveMomentum.H"
          #include "solvefn_ofT.H"
+ //       Info <<" QQQQQQQQQQQQQ"<< endl;
  //        #include "solveAirScalars.H" 
+   //     Info <<"wwwwwwwwww"<< endl;
  //        #include "solveFlux.H"
         runTime.write();
         Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
